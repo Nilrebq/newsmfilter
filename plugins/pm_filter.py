93b11baffdce70f4de0718835b73b681b5261abe
DRAGON-PRO <116484443+DRAGON-PRO@users.noreply.github.com>
2023-01-01 21:31:10 +0530

    ⚡𝙷𝙱⚡

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -725,7 +725,7 @@ async def auto_filter(client, msg, spoll=False):
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"🗓 1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
-             InlineKeyboardButton(text="ɴᴇꜱᴛ ▷", callback_data=f"next_{req}_{key}_{offset}")]
+             InlineKeyboardButton(text="ɴᴇxᴛ ▷", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
