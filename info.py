4f065d1b2c0b63159654f4e8002b6e46836475fd
DRAGON-PRO <116484443+DRAGON-PRO@users.noreply.github.com>
2023-01-01 22:04:58 +0530

    ⚡𝙷𝙱⚡

--- info.py
+++ info.py
@@ -35,8 +35,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://flims:flims@cluster0.vaa4txt.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "flims")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hulk:hulk@cluster0.jvggb8f.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "hulk")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
