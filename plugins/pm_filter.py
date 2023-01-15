c3dae0b1ab20e477e87b050baac274af0a26cecc
DRAGON-PRO <116484443+DRAGON-PRO@users.noreply.github.com>
2023-01-15 19:30:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -547,8 +547,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             query.message.chat.id,
             query.message.id,
             InputMediaPhoto(random.choice(PICS))
-            
-          
+        )
+         await query.message.edit_text(
             text=script.BUTTON_TXT,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
